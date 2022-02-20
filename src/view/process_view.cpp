4722293b5d2bdab3cad0049c60d351f4b1e65f35
10606 <gav_sbor@mail.ru>
2022-02-20 14:47:19 +0300

    bugfix: play by next if fake

--- src/view/process_view.cpp
+++ src/view/process_view.cpp
@@ -402,7 +402,7 @@ ret_code view::new_song_or_repeat ()
 {
     if (!audio_ctl->audio_file.is_fake())
         reuse_mad();
-    else
+    else if (next_playlist.value.is_fake())
         return 0;
 
     if (audio_ctl->pause_status == pause_status_t::soft_pause)
